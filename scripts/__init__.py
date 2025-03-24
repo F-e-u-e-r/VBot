51edd8b47a4e659d82bb2481f5cb378ffe7ed454
F-e-u-e-r <feuer@shsl.world>
2025-03-24 15:10:02 +0800

    Add scripts package initialization file

---
+++ scripts/__init__.py
@@ -0,0 +1,6 @@
+"""
+Command-line scripts for VBot application management.
+
+This package contains utility scripts for managing the VBot application
+from the command line, such as user management.
+"""
